f5d22c3c5b1a7aded722cefa25d485cb54ce7d37
Zoltán H <zoltan.h@wavemyth.com>
2023-01-26 16:16:03 +0200

    first commit

---
+++ js/intersection-obs.js
@@ -0,0 +1,15 @@
+// const sectionColors = {
+
+// }
+
+
+// <script>
+// const sectionColors = {
+//   one: #...,
+//   two: #...
+// }
+
+// {
+//   event.target.classList.add(sectionColors[event.target.getAttribute("id")]);
+// }
+// </script>
