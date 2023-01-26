01c3f3e00db741c26ba2214a8b3f2fa9cd262c95
Zoltán H <zoltan.h@wavemyth.com>
2023-01-26 17:10:10 +0200

    added dynamic bg color and text color for h2 elements

--- js/intersection-obs.js
+++ js/intersection-obs.js
@@ -1,13 +1,32 @@
-// const sectionColors = {
+const sectionColors = {
+  six: "#bee9e8ff",
+  five: "#62b6cbff",
+  four: "#5fa8d3ff",
+  three: "#5798bd",
+  two: "#518eb1",
+  one: "#4e86a7",
+}
 
-// }
+// const observer = new IntersectionObserver(
+//   (entries) => {
+//     entries.forEach((entry, index) => {
+//       const intersecting = entry.isIntersecting;
+//       console.log(entry)
+//       if (intersecting) {
+//         entry.target.style.setProperty("--sectionTitleText", sectionColors[entry.target.getAttribute("id")]);
+//         entry.target.style.setProperty("background", `rgba(255, 255, 255, .${entry.target.dataset.alpha})`);
+//       }
+//     })
+//   }
+// );
 
+const sectionTitles = document.querySelectorAll("h2");
+sectionTitles.forEach(sectionTitle => {
+  // observer.observe(sectionTitle);
+  sectionTitle.style.setProperty("--sectionTitleText", sectionColors[sectionTitle.getAttribute("id")]);
+  sectionTitle.style.setProperty("background", `rgba(255, 255, 255, .${sectionTitle.dataset.alpha})`);
+});
 
-// <script>
-// const sectionColors = {
-//   one: #...,
-//   two: #...
-// }
 
 // {
 //   event.target.classList.add(sectionColors[event.target.getAttribute("id")]);
